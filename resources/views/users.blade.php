32a329f64b6099e34a58880dd9e22acf6f6942f1
dydy <dydy.gregoire88120@gmail.com>
2020-02-13 17:15:03 +0100

    Ajout bouton liste utilisateur

---
+++ resources/views/users.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@section('title', 'users')
+    @section('content')
+
+    <h3>Liste des utilisateurs :</h3>
+    <ul class="liste-users">
+    @foreach ($users as $user)
+    <a href="/user/{{ $user->id }}" class="case-users bg-white mb-4 shadow">
+        <span>{{ $user->nom }}</span>
+    </a>
+    @endforeach
+
+</ul>
+
+@endsection

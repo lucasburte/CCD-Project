56d46827d4f8a2a7c15b932c175c7aad3efafc43
Rania Ben Said <raniabensaid.rbs@gmail.com>
2020-02-13 14:07:36 +0100

    Controlleur de créneau fonctionnel

--- app/Http/Controllers/Creneau/CreneauController.php
+++ app/Http/Controllers/Creneau/CreneauController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Creneau;
 
 
 use App\Database\Models\Creneau;
+use PhpParser\Node\Stmt\Foreach_;
 
 class CreneauController
 {
@@ -13,30 +14,69 @@ class CreneauController
 
    public function __construct(){}
 
+
+   //permet à la classe de trouver un créneau dans la base
+   public function trouverCreneau($jour,$semaine,$deb,$fin){
+       $creneau = Creneau::where('jour','=',$jour,'and','semaine','=',$semaine,'and','debut','=',$deb,'and','fin','=',$fin)->first();
+        return $creneau ;
+   }
+
     public function nouveauCreneau($jour,$semaine,$deb,$fin){
         $creneau = new Creneau() ;
         //verifier si le creneau n'est pas déjà utilisé ou contenu dans un autre créneau
-        if(Creneau::where('jour','=', $jour)->first() && Creneau::where('semaine','=',$semaine)){
-            //verifier que l'heure n'est pas contenue dans un autre horaire
-            echo "erreur, un creneau existe deja pour ce jour !" ;
-        }else{
+        $crenSemaine = Creneau::where('semaine','=',$semaine)->get();
+        if($crenSemaine != null){
+            foreach($crenSemaine as $cren){
+                if($cren->jour != $jour){
+                    //retirer le creneau
+                    unset($cren);
+                }
+            }
+            foreach($crenSemaine as $cren1){
+                if($cren->dateDebut > $deb && $cren->dateDebut > $fin){
+                    unset($cren) ;
+                }
+            }
+            foreach($crenSemaine as $cren1){
+                if($cren->dateFin > $deb && $cren->dateFin > $fin){
+                    unset($cren) ;
+                }
+            }
+        }
             $creneau->jour = $jour ;
             $creneau->semaine = $semaine ;
             $creneau->heureDebut = $deb ;
             $creneau->heureFin = $fin ;
-        }
 
         $creneau->save();
     }
-
+//permet à l'utilisateur de supprimer un creneau
     public function supprimerCreneau($jour,$semaine,$deb,$fin){
-     $creneau = Creneau::where('jour','=',$jour,'and','semaine','=',$semaine,'and','debut','=',$deb,'and','fin','=',$fin)->first();
-     //supprimer le creneau
+       //trouver le  creneau concerné
+        $creneau =  $this->trouverCreneau($jour,$semaine,$deb,$fin);
+       //supprimer le creneau
         $creneau->delete();
     }
 
-    public function modifierCreneau($jour,$semaine,$deb,$fin){
 
+    //permet à l'utilisateur de supprimer le creneau
+    public function modifierCreneau($creneau,$jour,$semaine,$deb,$fin){
+
+       if($creneau->jour != $jour){
+           $creneau->jour = $jour ;
+       }
+
+       if($creneau->semaine != $semaine){
+           $creneau->semaine = $semaine ;
+       }
+
+       if($creneau->dateDebut != $deb){
+           $creneau->dateDebut = $deb ;
+       }
+
+        if($creneau->dateFin != $fin){
+            $creneau->dateFin = $fin ;
+        }
     }
 
 }

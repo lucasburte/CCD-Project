a8d103ed06cbe67d2b332096bc4c90262c969423
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-02-13 10:59:26 +0100

    Ajout TODO

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -14,6 +13,6 @@ class LoginController extends Controller
     public function login($request) {
         $name = $request->input('name');
 
-        return response("{'name':'$name'}", 200)->header('Content-Type', 'application/json');
+        // TODO: handle connection
     }
 }

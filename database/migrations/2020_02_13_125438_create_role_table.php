dbc3785612a7468eb4090b309cfd598b1eb50d01
lucasburte <lucas.burte@hotmail.fr>
2020-02-13 14:00:25 +0100

    creation tables

--- database/migrations/2020_02_13_125438_create_role_table.php
+++ database/migrations/2020_02_13_125438_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('label', 128);
             $table->timestamps();
         });
     }
@@ -30,6 +27,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('role');
     }
 }
+
+

b6dffcf7e22f955b07cf4c96ab3676492a6b41e7
dydy <dydy.gregoire88120@gmail.com>
2020-02-13 15:13:14 +0100

    Redirection /board

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -44,7 +44,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+      <a class="nav-link" href="{{ route('board') }}">
          <i class="fas fa-laugh-wink"></i>
           <span>Board</span></a>
       </li>

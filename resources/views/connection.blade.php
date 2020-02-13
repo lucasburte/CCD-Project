3b75bb024aa11b22a19f43964c163d0fd30c251b
lucasburte <lucas.burte@hotmail.fr>
2020-02-13 11:48:12 +0100

    add front content

--- resources/views/connection.blade.php
+++ resources/views/connection.blade.php
@@ -1,119 +1,25 @@
-@extend('master')
+@extends('master')
 
-@section('title', 'Connexion')
+@section('title', 'connexion')
     @section('content')
-        <div class="ui">
-            <nav class="navbar app">App bar</nav>
-            <nav class="navbar board">Board bar</nav>
-            <div class="lists">
-                <div class="list">
-                    <header>List header</header>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
-                <div class="list">
-                    <header>Header</header>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>
-                            <img src="holder.js/300x150?auto=yes&bg=#ccc" alt="">
-                            Lorem ipsum dolor sit amet
-                        </li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
-                <div class="list">
-                    <header>Another List</header>
-                    <ul>
-                        <li>Just some text</li>
-                        <li>Just some text</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
-                <div class="list">
-                    <header>Header</header>
-                    <ul>
-                        <li>
-                            <img src="holder.js/600x400?auto=yes&bg=#ccc" alt="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        </li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>
-                            <img src="holder.js/150x150?auto=yes&bg=#ccc" alt="">
-                            Lorem ipsum dolor sit amet
-                        </li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
-                <div class="list">
-                    <header>Another List</header>
-                    <ul>
-                        <li>Just some text</li>
-                        <li>Yet another card</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
-                <div class="list">
-                    <header>Header</header>
-                    <ul>
-                        <li>
-                            <img src="holder.js/150x150?auto=yes&bg=#ccc" alt="">
-                            Lorem ipsum dolor sit amet
-                        </li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>
-                            <img src="holder.js/600x400?auto=yes&bg=#ccc" alt="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        </li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet metus laoreet, ut condimentum</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Lorem ipsum dolor sit amet</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
-                <div class="list">
-                    <header>Another List</header>
-                    <ul>
-                        <li>Just some text</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis enim sit amet</li>
-                        <li>Some more text</li>
-                        <li>Some more text</li>
-                    </ul>
-                    <footer>Add a card...</footer>
-                </div>
+        <form action="/auth" method="post">
+            @csrf
+            <p>Selectionnez un utilisateur</p>
+
+            <li><a href="#">Lien menu 1</a>
+                <ul>
+                    <li><a href="#">lien sous menu 1</a></li>
+                    <li><a href="#">lien sous menu 1</a></li>
+                    <li><a href="#">lien sous menu 1</a></li>
+                    <li><a href="#">lien sous menu 1</a></li>
+                </ul>
+            </li>
+{{--             @foreach ($users as $user)
+            <div>
+                <input type="radio" id="huey" name="users" value="huey">
+                <label for="huey"></label>
             </div>
-        </div>
+            @endforeach --}}
+            
+        </form>
     @endsection
-@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet/scss" type="text/css" href="../../public/css/siteBoard.scss">
         {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
         <title>@yield('title')</title>
     </head>

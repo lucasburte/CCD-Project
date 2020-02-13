4976600274a6d93a8a17bbf1343bb1676ba922bf
Rania Ben Said <raniabensaid.rbs@gmail.com>
2020-02-13 11:45:14 +0100

    on peut ajouter un nouveau créneau

---
+++ app/Http/Controllers/Creneau/CreneauController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Http\Controllers\Creneau;
+
+
+
+use App\Database\Models\Creneau;
+
+class CreneauController
+{
+
+
+   public function __construct(){}
+
+    public function nouveauCreneau($jour,$semaine,$deb,$fin){
+        $creneau = new Creneau() ;
+
+        //verifier si le creneau n'est pas déjà utilisé ou contenu dans un autre créneau
+
+        if(Creneau::where('jour','=', $jour)->first() && Creneau::where('semaine','=',$semaine)){
+            //verifier que l'heure n'est pas contenue dans un autre horaire
+            echo "erreur, un creneau existe deja pour ce jour !" ;
+        }else{
+            $creneau->jour = $jour ;
+            $creneau->semaine = $semaine ;
+            $creneau->heureDebut = $deb ;
+            $creneau->heureFin = $fin ;
+        }
+
+        $creneau->save();
+    }
+
+}

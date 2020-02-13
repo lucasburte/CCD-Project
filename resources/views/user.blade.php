28b6d71d865d2e25b24899f49ff39a441f64c067
dydy <dydy.gregoire88120@gmail.com>
2020-02-13 18:26:03 +0100

    Modif view user

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 
-@section('title', 'user')
+@section('title', 'user', '')
     @section('content')
     <p>Nom de l'utilisateur : {{ $user->nom }}</p> 
     <p>Role : {{ $role->label }}</p>  
